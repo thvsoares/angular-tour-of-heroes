e2b3ad4ccfa0bb9e6c04aeb1636fabbc6c1f0850
Thiago Soares <thiagomullet@gmail.com>
2018-08-26 21:48:42 -0300

    route tutorial review

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
