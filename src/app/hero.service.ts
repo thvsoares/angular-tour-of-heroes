7c15cbcd2b46f1d8cea3f689a435001ac5a15f08
Thiago Soares <thiagomullet@gmail.com>
2018-08-27 11:01:23 -0300

    get heroes tap log

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,6 +19,7 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(heroes => this.log('fetched heroes')),
         catchError(this.handleError('getHeroes', []))
       );
   }
